b8765fefc66b88f6163af8da610ea7587ad284db
izabel <izabel.nascimento@nees.ufal.br>
2025-07-25 00:06:29 -0300

    refactor: Removendo classes não utilizadas

--- src/main/java/com/izabel/health/data/etl/loader/ValorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.izabel.health.data.etl.loader;
-
-import com.izabel.health.data.etl.model.Value;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ValorRepository extends JpaRepository<Value, Long> {}
